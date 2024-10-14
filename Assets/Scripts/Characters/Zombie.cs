d253f6c71d90588c87f939a59688683cad3e8191
Kenan Ege <kenanegee@gmail.com>
2024-10-14 17:12:26 +0300

    Update Zombie.cs

--- Assets/Scripts/Characters/Zombie.cs
+++ Assets/Scripts/Characters/Zombie.cs
@@ -12,71 +12,72 @@ using UnityEngine;
 
 public class Zombie : CharacterController
 {
-    float uuugh     =       0;
-        Vector2 braaaaains = new Vector2(5, 8);
-    Vector2Int          uhghbrains = new Vector2Int(3, 7);
+    float uuugh = 0;
+    Vector2 braaaaains = new Vector2(5, 8);
+    Vector2Int uhghbrains = new Vector2Int(3, 7);
+
     public override void StartCharacter()
     {
-        myCurrentTile   =             Grid.Instance.GetClosest(transform.position);
+        myCurrentTile = Grid.Instance.GetClosest(transform.position);
         base.StartCharacter();
     }
+
     public override void UpdateCharacter()
     {
         base.UpdateCharacter();
 
         if (uuugh > 0)
         {
-                         uuugh -= Time.deltaTime;
+            uuugh -= Time.deltaTime;
         }
-                else
-            {
-                        ughBrainBrainugh();
+        else
+        {
+            ughBrainBrainugh();
         }
     }
 
     public void ughBrainBrainugh()
     {
-        uuugh =  Random.Range(braaaaains.x,         braaaaains.y);
-                        brainbrainzombiebrain();
+        uuugh = Random.Range(braaaaains.x, braaaaains.y);
+        brainbrainzombiebrain();
     }
 
     public void brainbrainzombiebrain()
     {
-            int         braaaaaains =           Random.Range(0, 4);
-        int ughghhhhhh =    Random.Range(uhghbrains.x, uhghbrains.y);
+        int braaaaaains = Random.Range(0, 4);
+        int ughghhhhhh = Random.Range(uhghbrains.x, uhghbrains.y);
 
-        List<Grid.Tile>     brains =    new List<Grid.Tile>();
-
-        Vector2Int bruhains =       Vector2Int.zero;
+        List<Grid.Tile> brains = new List<Grid.Tile>();
+        Vector2Int bruhains = Vector2Int.zero;
 
         switch (braaaaaains)
         {
-                        case 0:
-                bruhains =  new Vector2Int(1, 0 );
+            case 0:
+                bruhains = new Vector2Int(1, 0);
                 break;
             case 1:
-                bruhains = new          Vector2Int(-1, 0);
+                bruhains = new Vector2Int(-1, 0);
                 break;
-                            case 2:
-                            bruhains = new Vector2Int(0, 1);
+            case 2:
+                bruhains = new Vector2Int(0, 1);
                 break;
-                case 3:
+            case 3:
                 bruhains = new Vector2Int(0, -1);
-                    break;
+                break;
         }
 
-        for (int brainses = 1;              brainses < ughghhhhhh + 1; brainses++)
+        for (int brainses = 1; brainses < ughghhhhhh + 1; brainses++)
         {
-                        Grid.Tile brainbrains = Grid.Instance.TryGetTile(new Vector2Int(myCurrentTile.x + (bruhains.x * brainses), myCurrentTile.y + (bruhains.y * brainses)));
+            Grid.Tile brainbrains = Grid.Instance.TryGetTile(new Vector2Int(myCurrentTile.x + (bruhains.x * brainses), myCurrentTile.y + (bruhains.y * brainses)));
             if (brainbrains != null)
             {
-                    if (!brainbrains.occupied)
+                if (!brainbrains.occupied)
                 {
-                                        brains.Add(brainbrains);
-                    }
+                    brains.Add(brainbrains);
+                }
             }
         }
 
-            SetWalkBuffer(brains);
+        SetWalkBuffer(brains);
     }
 }
